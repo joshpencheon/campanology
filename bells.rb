9e3d765ebe72440006e8ea3989ac3c151dd25831
Josh Pencheon <josh.pencheon@googlemail.com>
2012-11-11 20:55:59 +0000

    Add 'valid change' finding logic

--- bells.rb
+++ bells.rb
@@ -16,6 +16,7 @@ require_relative 'lib/extent_checker'
 require_relative 'lib/extent_builder'
 
 require_relative 'lib/cycle_finder'
+require_relative 'lib/change_finder'
 
 require_relative 'lib/graph_builder'
 require_relative 'lib/complete_graph'
@@ -56,20 +57,20 @@ puts "**** [Hamiltonian Cycle Finding] ****"
 finder = CycleFinder.from_graph_builder(builder)
 
 puts "Original graph connected enough for guaranteed results?: " + finder.conditions_met?.to_s
-results = finder.seek!
-
-cycles = []
-results.each do |starting_node, node_results|
-  node_results ||= {}
-  
-  (node_results[:cycles] || []).each do |cycle|
-    cycles << cycle
-  end
-end
-
-puts "  cycle count: #{cycles.length}"
-puts "  valid cycle count: #{cycles.select { |cycle| ExtentChecker.new(cycle).check }.length}"
-puts "  unique cycle count: #{cycles.uniq.length}"
+# results = finder.seek!
+# 
+# cycles = []
+# results.each do |starting_node, node_results|
+#   node_results ||= {}
+#   
+#   (node_results[:cycles] || []).each do |cycle|
+#     cycles << cycle
+#   end
+# end
+# 
+# puts "  cycle count: #{cycles.length}"
+# puts "  valid cycle count: #{cycles.select { |cycle| ExtentChecker.new(cycle).check }.length}"
+# puts "  unique cycle count: #{cycles.uniq.length}"
 
 puts "**** [MIDI Sequencing] ****"
 
@@ -83,4 +84,10 @@ MidiCreator.new(tracks).export!
 
 puts "Extent MIDI outputted!"
 
-puts "*****************"
+puts "**** [Change finder] ****"
+
+bell_count = 7
+puts "On #{bell_count} bells, expect #{ChangeFinder.count_for(bell_count)} valid changes:"
+ChangeFinder.new(8).valid_changes_in_notation.each_with_index { |change, index| puts "  #{index+1}:  #{change.inspect}" }
+
+puts "*************************"
